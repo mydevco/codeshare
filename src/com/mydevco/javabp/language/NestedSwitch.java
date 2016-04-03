14ad88b66eb0263e76109fe2f936a30efc3e6f45
mydevco <mydevco@gmail.com>
2016-04-03 17:43:59 -0400

    Added more example

--- src/com/mydevco/javabp/language/NestedSwitch.java
+++ src/com/mydevco/javabp/language/NestedSwitch.java
@@ -2,10 +2,14 @@ package com.mydevco.javabp.language;
 
 public class NestedSwitch {
 
-	private static final String BURLINGTON = "BURLINGTON";
-	private static final String BOSTON = "BOSTON";
-	private static final String NC = "NC";
-	private static final String MA = "MA";
+	private static final String BURLINGTON_S = "BURLINGTON";
+	private static final String BOSTON_S = "BOSTON";
+	private static final String NC_S = "NC";
+	private static final String MA_S = "MA";
+	
+	private static enum STATE{MA, NC};
+	
+	private static enum TOWN{BURLINGTON, BOSTON};
 
 	public static void main(String[] args) {
 		if (args.length < 2) {
@@ -15,16 +19,57 @@ public class NestedSwitch {
 
 		String state = args[0];
 		String stateUpper = state.toUpperCase();
+		STATE stateEnum = STATE.valueOf(state.toUpperCase());
 		String town = args[1];
 		String townUpper = town.toUpperCase();
+		TOWN townEnum = TOWN.valueOf(town.toUpperCase());
+		
 		long start = System.nanoTime();
 		switch (stateUpper) {
+		case NC_S:
+			System.out.println("Welcome to North Carolina!");
+			break;
+		case MA_S:
+			System.out.println("Welcome to Massacusettes!");
+			switch (townUpper) {
+			case BOSTON_S:
+				System.out.println("Capital Town");
+				break;
+			case BURLINGTON_S:
+				System.out.println("Great Town");
+				break;
+			default:
+				System.out.println("All town are great in MA");
+			}
+			break;
+
+		}
+		System.out.println("Time taken with nested String switch =" + (System.nanoTime() - start));
+
+		start = System.nanoTime();
+		if (stateUpper.equals(NC_S)) {
+			System.out.println("Welcome to North Carolina!");
+
+		} else if (stateUpper.equals(MA_S)) {
+			System.out.println("Welcome to Massacusettes!");
+			if (townUpper.equals(BOSTON_S)) {
+				System.out.println("Capital Town");
+			} else if (townUpper.equals(BURLINGTON_S)) {
+				System.out.println("Great Town");
+			} else {
+				System.out.println("All town are great in MA");
+			}
+		}
+		System.out.println("Time taken with nested if else=" + (System.nanoTime() - start));
+		
+		start = System.nanoTime();
+		switch (stateEnum) {
 		case NC:
 			System.out.println("Welcome to North Carolina!");
 			break;
 		case MA:
 			System.out.println("Welcome to Massacusettes!");
-			switch (townUpper) {
+			switch (townEnum) {
 			case BOSTON:
 				System.out.println("Capital Town");
 				break;
@@ -37,23 +82,30 @@ public class NestedSwitch {
 			break;
 
 		}
-		System.out.println("Time taken =" + (System.nanoTime() - start));
-
+		System.out.println("Time taken with nested enum Switch=" + (System.nanoTime() - start));
+		
 		start = System.nanoTime();
-		if (stateUpper.equals(NC)) {
+		switch (stateEnum.ordinal()) {
+		case 1:
 			System.out.println("Welcome to North Carolina!");
-
-		} else if (stateUpper.equals(MA)) {
+			break;
+		case 0:
 			System.out.println("Welcome to Massacusettes!");
-			if (townUpper.equals(BOSTON)) {
+			switch (townEnum.ordinal()) {
+			case 1:
 				System.out.println("Capital Town");
-			} else if (townUpper.equals(BURLINGTON)) {
+				break;
+			case 0:
 				System.out.println("Great Town");
-			} else {
+				break;
+			default:
 				System.out.println("All town are great in MA");
 			}
+			break;
+
 		}
-		System.out.println("Time taken =" + (System.nanoTime() - start));
+		System.out.println("Time taken with nested enum Switch=" + (System.nanoTime() - start));
+
 
 	}
 
